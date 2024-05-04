5b966e37b990645dcb1613d7ba13d47712555708
SaraMAlshareef <148724281+SaraMAlshareef@users.noreply.github.com>
2024-05-04 22:57:07 +0300

    Update PlaygroundFacade.java

--- PlaygroundFacade.java
+++ PlaygroundFacade.java
@@ -1,1 +1,18 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
 
+public class PlaygroundFacade {
+    private Playground playground;
+
+    public PlaygroundFacade() {
+        this.playground = new Playground();
+    }
+
+    public void printCard(Card card, int n, int numCard) {
+        playground.printCard(card, n, numCard);
+    }
+
+    public void printPlayground(ArrayList<Player> players, LinkedList<Card> cards, int turn, int playerTurn, int choose) {
+        playground.printPlayground(players, cards, turn, playerTurn, choose);
+    }
+}
